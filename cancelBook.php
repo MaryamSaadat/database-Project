b7a438350d6bb6623bf999dfc4cf249ed621ee32
Umme Ammara <73883745+umme-ammara@users.noreply.github.com>
2020-12-04 06:29:29 +0500

    Add files via upload
    Contains HTML code that takes in input: username and bookingID to cancel a booking. The form sends data to cancelValidate.php

---
+++ cancelBook.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Cancel Booking</title>
+  <link rel="stylesheet" href="./style3.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<h1> CANCEL BOOKING </h1>
+
+<p>
+<h2> Enter your username and booking ID in order to confirm the cancellation. </h2>
+</p>
+
+
+ <form action="cancelValidate" method ="post">
+ <p>
+   <label for="username">Username:</label>
+<input type="text" name="user_name" id="username" />
+ </p>
+
+ <p>
+   <label for="booking_id">Book ID:</label>
+<input type="number" name="book_id" id="booking_id" />
+ </p>
+ 
+<input type="submit" value = "Confirm">
+</form>
+<!-- partial -->
+  
+</body>
+</html>
