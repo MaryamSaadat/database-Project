f2b94df84503d9f37cc4b5523f38326a621f0ab7
Umme Ammara <73883745+umme-ammara@users.noreply.github.com>
2020-12-08 00:45:00 +0500

    Update guest.php

--- guest.php
+++ guest.php
@@ -11,20 +11,53 @@
 <body>
 <!-- partial:index.partial.html -->
 	<h1>HOTEL TRANSYLVANIA</h1>
+	<?php
+	if (isset( $_GET['successfulCreation']) ) 
+        {
+            echo "<p>YOU HAVE BOOKED A ROOM!.</p>";
+		}
+		if (isset( $_GET['successfulCancel']) ) 
+        {
+            echo "<p>YOU HAVE CANCELLED BOOKING.</p>";
+        }
+	?>
 
 	<div class = "s">
+		<p> 
+		  <form method="post" action="bookRoom.php">
+			<button type="submit">BOOK ROOM</button>
+		  </form>
+		
+		</p>
 		<p>
-			<button type="button">BOOK ROOM</button>
-			<button type="button">CANCEL BOOKING</button>
+		<form method="post" action="cancelBook.php">
+			<button type="submit">CANCEL BOOKING</button>
+		</form>
+		
+		</p>
+
+		<p> 
+		  <form method="post" action="orderFood.php">
+			<button type="submit">ORDER ROOMSERVICE</button>
+		  </form>
+		</p>
+
+		<p> 
+		  <form method="post" action="">
+			<button type="submit">INVOICE GENERATION</button>
+		  </form>
 		</p>
 
 		<p>
-			<button type="button">INVOICE GENERATION</button>
-			<button type="button">ONLINE PAYMENT</button>
+		<form method="post" action="">
+			<button type="submit">ONLINE PAYMENT</button>
+		</form>
 		</p>
 
 		<p>
-			<button type="button">VIEW REWARDS</button>
+		<form method="post" action="">
+			<button type="submit">VIEW REWARDS</button>
+		</form>
 		</p>
 
 	</div>
