2ab956486bd6d79951ca59242c35b7ff03812116
Umme Ammara <73883745+umme-ammara@users.noreply.github.com>
2020-12-13 13:03:56 +0500

    Update foodValidate.php
    Error conditions handled and payments handled.

--- foodValidate.php
+++ foodValidate.php
@@ -20,12 +20,45 @@ if (isset($_POST['o_date']) and isset($_POST['book_id']) and isset($_POST['food_
     $food = $_POST['food_id'];
     //echo $food;
 
-    $sql = "INSERT INTO `roomservice` (`orderID`,`bookingID`,`foodID`,`orderDate`) VALUES (NULL,'$book', '$food', '$order')";
+    $sql_0 = "SELECT * FROM `bookings` WHERE `bookingID`='$book'";
+    $res_0 = mysqli_query($conn, $sql_0) or die(mysqli_error($conn));
+    $num_0 = mysqli_num_rows($res_0);
+
+    if ($num_0==0){
+       
+   
+        header('Location:orderFood.php?Error=1');
+    }
+
+
+
+    $sql = "INSERT INTO `roomservice` (`orderID`,`bookingID`,`foodID`,`orderDate`) VALUES (NULL, '$book', '$food', '$order')";
     $q = mysqli_query ($conn,$sql) or die(mysqli_error($conn));
+
     if ($q==1)
     {
-        //echo "your order have been placed";
-        header('Location:orderDone.php');
+        // EXTRACTING ORDER ID
+        $sql2 = "SELECT orderID FROM `roomservice` WHERE bookingId ='$book' AND foodID = '$food' AND orderDate = '$order'";
+		$res2 = mysqli_query($conn, $sql2) or die(mysqli_error($conn));
+
+		$oid = $res2 -> fetch_assoc();
+        $oid = $oid['orderID'];
+
+        // EXTRACTING PRICE OF FOOD ID
+        $sql3 = "SELECT price FROM `menu` WHERE foodID = '$food'";
+		$res = mysqli_query($conn, $sql3) or die(mysqli_error($conn));
+
+		$price = $res -> fetch_assoc();
+        $price = $price['price'];
+        
+        // PUSHING INTO PAYMENTS TABLE
+        $sql4 = "INSERT INTO `paymentsservice` (`bookingID`, `orderID`, `price`) VALUES ('$oid', '$book', '$price')";
+        $quer = mysqli_query ($conn,$sql4) or die(mysqli_error($conn)); 
+        
+        if ($quer == 1) {
+            //echo "your order have been placed";
+            header('Location:orderDone.php');
+        }
     }
 
 
